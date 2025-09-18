7108386831f92369f874498ccbc495f39e120226
PrinceMishra7 <iprincemisra@gmail.com>
2025-09-18 10:04:19 +0530

    setting up frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
-import App from './App.jsx'
+import Home from './pages/Home.jsx'
+import Upload from './pages/Upload.jsx'
+import Analysis from './pages/Analysis.jsx'
+// import TestIntegration from './pages/TestIntegration.jsx'
+
+const router = createBrowserRouter([
+  { path: '/', element: <Home /> },
+  { path: '/upload', element: <Upload /> },
+  { path: '/analysis', element: <Analysis /> },
+  
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
